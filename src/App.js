55f64fd46db917811a1fbe6749063ccda3904c56
Nguyen-Gray Khoa <khoanguyengray@gmail.com>
2022-05-24 22:29:58 -0700

    add useCallback

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch as Routes , Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch as Routes , Route } from 'react-router-dom'
 // import pages
 import Home from './pages/Home'
 import About from './pages/About'
--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useCallback } from 'react';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
+
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const AppContext = React.createContext();
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a');
   const [cocktails, setCocktails] = useState([]);  
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback(async () => {
     setLoading(true);
     try {
        const response = await fetch(`${url}${searchTerm}`);
@@ -30,11 +30,11 @@ const AppProvider = ({ children }) => {
       setLoading(false);
       console.log(error)
     }
-  }
+  }, [searchTerm])
 
   useEffect(() => {
     fetchDrinks();
-  },[searchTerm])
+  },[searchTerm, fetchDrinks])
 
   return <AppContext.Provider value={{loading, cocktails, setSearchTerm}}>{children}</AppContext.Provider>;
 };
